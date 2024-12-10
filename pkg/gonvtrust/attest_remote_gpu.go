78b19c4f2f88edd96fc3989c37fe487998cec2d5
Vadim Gribanov <vadim.gribanov@fluxon.com>
2024-12-10 13:24:30 +0100

    Return error

--- pkg/gonvtrust/attest_remote_gpu.go
+++ pkg/gonvtrust/attest_remote_gpu.go
@@ -6,7 +6,6 @@ import (
 	"encoding/base64"
 	"encoding/pem"
 	"fmt"
-	"log"
 
 	"github.com/NVIDIA/go-nvml/pkg/nvml"
 )
@@ -32,24 +31,24 @@ func NewGpuAttester(testMode bool) *GpuAttester {
 	}
 }
 
-func (g *GpuAttester) GetRemoteEvidence(nonce int) []RemoteEvidence {
+func (g *GpuAttester) GetRemoteEvidence(nonce int) ([]RemoteEvidence, error) {
 	ret := g.nvmlHandler.Init()
+
+	if ret != nvml.SUCCESS {
+		return nil, fmt.Errorf("Unable to initialize NVML: %v", nvml.ErrorString(ret))
+	}
 
 	computeState, ret := g.nvmlHandler.SystemGetConfComputeState()
 	if ret != nvml.SUCCESS {
-		log.Fatalf("Unable to get compute state: %v", nvml.ErrorString(ret))
+		return nil, fmt.Errorf("Unable to get compute state: %v", nvml.ErrorString(ret))
 	}
 	if computeState.CcFeature != nvml.CC_SYSTEM_FEATURE_ENABLED {
-		log.Fatalf("Confidential computing is not enabled")
-	}
-
-	if ret != nvml.SUCCESS {
-		log.Fatalf("Unable to initialize NVML: %v", nvml.ErrorString(ret))
+		return nil, fmt.Errorf("Confidential computing is not enabled")
 	}
 
 	count, ret := g.nvmlHandler.DeviceGetCount()
 	if ret != nvml.SUCCESS {
-		log.Fatalf("Unable to get device count: %v", nvml.ErrorString(ret))
+		return nil, fmt.Errorf("Unable to get device count: %v", nvml.ErrorString(ret))
 	}
 
 	var remoteEvidence []RemoteEvidence
@@ -58,22 +57,22 @@ func (g *GpuAttester) GetRemoteEvidence(nonce int) []RemoteEvidence {
 		device, ret := g.nvmlHandler.DeviceGetHandleByIndex(i)
 
 		if ret != nvml.SUCCESS {
-			log.Fatalf("Unable to get device at index %d: %v", i, nvml.ErrorString(ret))
+			return nil, fmt.Errorf("Unable to get device at index %d: %v", i, nvml.ErrorString(ret))
 		}
 
 		deviceArchitecture, ret := device.GetArchitecture()
 		if ret != nvml.SUCCESS {
-			log.Fatalf("Unable to get architecture of device at index %d: %v", i, nvml.ErrorString(ret))
+			return nil, fmt.Errorf("Unable to get architecture of device at index %d: %v", i, nvml.ErrorString(ret))
 		}
 
 		if deviceArchitecture != nvml.DEVICE_ARCH_HOPPER {
-			log.Fatalf("Device at index %d is not supported", i)
+			return nil, fmt.Errorf("Device at index %d is not supported", i)
 		}
 
 		report, ret := device.GetConfComputeGpuAttestationReport()
 
 		if ret != nvml.SUCCESS {
-			log.Fatalf("Unable to get attestation report of device at index %d: %v", i, nvml.ErrorString(ret))
+			return nil, fmt.Errorf("Unable to get attestation report of device at index %d: %v", i, nvml.ErrorString(ret))
 
 		}
 
@@ -83,25 +82,25 @@ func (g *GpuAttester) GetRemoteEvidence(nonce int) []RemoteEvidence {
 		certificate, ret := device.GetConfComputeGpuCertificate()
 
 		if ret != nvml.SUCCESS {
-			log.Fatalf("Unable to get certificate of device at index %d: %v", i, nvml.ErrorString(ret))
+			return nil, fmt.Errorf("Unable to get certificate of device at index %d: %v", i, nvml.ErrorString(ret))
 		}
 
 		attestationCertChainData := certificate.AttestationCertChain[:certificate.AttestationCertChainSize]
 		certs := splitCertificates(attestationCertChainData)
 		err := verifyCertificateChain(certs)
 		if err != nil {
-			log.Fatalf("Failed to verify certificate chain: %v", err)
+			return nil, fmt.Errorf("Failed to verify certificate chain: %v", err)
 		}
 
 		encodedCertChain, err := convertAndBase64Encode(certs)
 		if err != nil {
-			log.Fatalf("Failed to encode certificate chain: %v", err)
+			return nil, fmt.Errorf("Failed to encode certificate chain: %v", err)
 		}
 
 		remoteEvidence = append(remoteEvidence, RemoteEvidence{evidence: encodedAttestationReport, certificate: encodedCertChain})
 	}
 
-	return remoteEvidence
+	return remoteEvidence, nil
 }
 
 func splitCertificates(chainData []byte) [][]byte {
