f23fabb3b6949018a0c80ff4b3c20968f128117e
Vadim Gribanov <vadim.gribanov@fluxon.com>
2024-12-30 17:22:50 +0100

    Add integration test for GPU

---
+++ pkg/gonvtrust/gpu_integration_test.go
@@ -0,0 +1,32 @@
+// go:build gpu_integration
+
+package gonvtrust
+
+import (
+	"crypto/rand"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGpuAttester_GetRemoteEvidence(t *testing.T) {
+	// Test that GetRemoteEvidence returns RemoteEvidence for each device
+	// and that the RemoteEvidence is valid.
+	// This test requires a GPU to be present.
+	// To run this test, use the following command:
+	// go test -tags=gpu_integration -v -run TestGpuAttester_GetRemoteEvidence
+	// This test will fail if no GPU is present.
+	attester := NewGpuAttester(false)
+	nonce := make([]byte, 32)
+	_, err := rand.Read(nonce)
+	if err != nil {
+		t.Fatalf("failed to generate nonce: %v", err)
+	}
+	evidence, err := attester.GetRemoteEvidence(nonce)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, evidence)
+	for _, remoteEvidence := range evidence {
+		assert.NotEmpty(t, remoteEvidence.Certificate)
+		assert.NotEmpty(t, remoteEvidence.Evidence)
+	}
+}
